4c5b56cb7ab98885eb2c3431f4497f21cdaf5fe4
Rahul Kumar <rahulkumarpahwa@gmail.com>
2026-09-13 14:58:33 +0530

    refactor(subscriber): streamline subscription logic and remove unnecessary variable

--- pubsub/src/subscriber.js
+++ pubsub/src/subscriber.js
@@ -2,18 +2,15 @@ import { Redis } from "ioredis";
 
 const subscriber = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
-const emailSubsciber = subscriber.subscribe(
-  "email:pubsub:channel",
-  async (err) => {
-    if (err) {
-      console.error("failed to subscribe: ", err.message);
-      return;
-    }
+subscriber.subscribe("email:pubsub:channel", async (err) => {
+  if (err) {
+    console.error("failed to subscribe: ", err.message);
+    return;
+  }
 
-    console.log("Subscribed successfully!");
-  },
-);
+  console.log("Subscribed successfully!");
+});
 
-emailSubsciber.on("message", async (channel, message) => {
+subscriber.on("message", async (channel, message) => {
   console.log("email recieved on", channel, " : ", message);
 });
